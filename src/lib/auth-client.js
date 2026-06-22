f07f7e6cf01d60e48dd03a33611f854c621633f4
mi-araf <arafmushfiq@gmail.com>
2026-06-23 00:32:59 +0600

    fixing oauth

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,16 +1,22 @@
-import { createAuthClient } from "better-auth/react";
+// import { createAuthClient } from "better-auth/react";
 
-export const API_URL =
-    process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+// export const API_URL =
+//     process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
 
-const getAuthBaseURL = () => {
-    if (typeof window !== "undefined") {
-        return window.location.origin;
-    }
+// const getAuthBaseURL = () => {
+//     if (typeof window !== "undefined") {
+//         return window.location.origin;
+//     }
 
-    return process.env.NEXT_PUBLIC_AUTH_URL || "http://localhost:3000";
-};
+//     return process.env.NEXT_PUBLIC_AUTH_URL || "http://localhost:3000";
+// };
 
+// export const authClient = createAuthClient({
+//     baseURL: getAuthBaseURL(),
+// });
+
+import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: getAuthBaseURL(),
-});
+    /** The base URL of the server (optional if you're using the same domain) */
+    baseURL: process.env.BETTER_AUTH_URL,
+})
